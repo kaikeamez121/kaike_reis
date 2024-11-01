00dace14f84daf843109d5080fd15c5aac1a7dbe
Otávio Augusto <otavioaugusto_2007@outlook.com>
2024-10-31 21:57:06 -0300

    revise na aula de sexta

--- relatorio.php
+++
@@ -1,9 +0,0 @@
-<?php
-//faz a requisicao com o vendor
-require_once 'vendor/autoload.php';
-//chama a classe Mpdf
-$mpdf= new \Mpdf\Mpdf();
-//escreve direto em HTML WriteHTML('<h1>Minha primeira pagina HTML em PDF</h1>');
-$mpdf->
-//faz o envio
-$mpdf->Output();
